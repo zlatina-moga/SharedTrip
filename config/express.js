7de1a7e058231068ab5279f95fb36e08e7838974
Zlatina Moga <zlatina.delcheva@gmail.com>
2021-12-13 11:58:10 +0200

    installed dependencies

--- config/express.js
+++ config/express.js
@@ -3,13 +3,11 @@ const hbs = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
-    app.engine('hbs', hbs ({
+    app.engine('hbs', hbs({
         extname: 'hbs'
-    }));
+    }))
     app.set('view engine', 'hbs');
     app.use('/static', express.static('static'));
     app.use(express.urlencoded({extended: true}));
-    app.use(cookieParser())
-
-    
+    app.use(cookieParser());
 }
